af64827716c7dbb84231f83e555b5d5d16780380
Palak-agarwal <agapalak@upenn.edu>
2020-10-19 20:59:29 -0400

    Update Assignment3.R

--- Assignment3.R
+++ Assignment3.R
@@ -22,13 +22,32 @@ policeDistricts_sf <-
   st_read("./Data/Current Police Districts.geojson") %>% 
   st_transform('ESRI:102241')
 
+policeDistricts_sf <- 
+  read.socrata("https://data.sfgov.org/resource/q52f-skbd.json") %>% 
+  mutate(geometry = the_geom.coordinates) %>%
+  st_as_sf() %>%
+  st_transform('ESRI:102241')
+
 assualt <- 
   read.socrata("https://data.sfgov.org/resource/tmnf-yvry.json") %>% 
-  filter(Category == "ASSAULT") %>%
-  st_as_sf(coords = c("X", "Y"), crs = 4326, agr = "constant")%>%
+  filter(category == "ASSAULT" & date > as.POSIXct("2016-01-01") & date < as.POSIXct("2017-01-01") ) %>%  
+  st_as_sf(coords = c("x", "y"), crs = 4326, agr = "constant")%>%
   st_transform('ESRI:102241') %>% 
   distinct()
 
+sfCounty <-
+  read.socrata("https://data.sfgov.org/resource/wamw-vt4s.json") %>%
+  filter(county == "San Francisco") %>%
+  mutate(geometry = the_geom.coordinates) %>%
+  st_sf() %>%
+  st_transform('ESRI:102241')
+
+sfCounty <-
+  st_read("./Data/Bay Area Counties.geojson") %>% 
+  st_transform('ESRI:102241')
+  
+
+stat_density2d
 
 
 
